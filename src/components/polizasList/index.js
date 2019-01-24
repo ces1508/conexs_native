d705478522178d812c9929150a43e76b7fc979b4
christian segura <ces1508@gmail.com>
2019-01-23 20:48:32 -0500

    item separator

--- src/components/polizasList/index.js
+++ src/components/polizasList/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import List from '../list'
 import Poliza from '../poliza'
+import { View } from 'react-native'
 
 const PolizasList = props => (
   <List
@@ -8,8 +9,11 @@ const PolizasList = props => (
     data={props}
     {...props}
     height={0.1}
+    ItemSeparatorComponent={() => <Separator />}
     renderItem={({ item }) => <Poliza {...item} navigation={props.navigation} />}
   />
 )
 
 export default PolizasList
+
+const Separator = props => (<View style={{ height: 1, backgroundColor: 'gray' }} />)
