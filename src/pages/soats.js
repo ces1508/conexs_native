36d12f4567095315348dfac8f8ee404c6ce2a78e
christian segura <ces1508@gmail.com>
2019-01-23 19:36:43 -0500

    integrate soats page with redux

--- src/pages/soats.js
+++ src/pages/soats.js
@@ -1,31 +1,18 @@
 import React, { Component } from 'react'
-import Datasource from '../api'
 import PolizasList from '../components/polizasList'
-import { getItem } from '../utils'
+import { connect } from 'react-redux'
+import { getPolizas } from '../actions/polizas/creators'
 
-export default class Soats extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      soats: []
-    }
-  }
+const mapStateToProps = state => ({ ...state.polizas })
+const mapsDispatchToProps = {
+  getPolizas
+}
 
-  componentDidMount () {
-    this.getPolizas()
-  }
-
-  async getPolizas () {
-    let user = await getItem('@user')
-    let { data, status } = await Datasource.getPolizas(user.item)
-    if (data.error) {
-      console.warn('tenemos un error')
-    } else {
-      this.setState({ soats: data.filter(item => item.formato === 'SOAT') })
-    }
-  }
-
+class Soats extends Component {
   render () {
-    return <PolizasList data={this.state.soats} navigation={this.props.navigation} />
+    let { soats } = this.props.soats
+    return <PolizasList data={soats} navigation={this.props.navigation} />
   }
 }
+
+export default connect(mapStateToProps, mapsDispatchToProps)(Soats)
